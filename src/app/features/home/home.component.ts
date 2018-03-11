4d84a613a83763c28f1b9eae67738f3d598b372c
Vadim <vv342915007@gmail.com>
2018-03-11 19:58:54 +0200

    change home component (show phone list)

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'tl-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public phoneList$: Observable<any>;
 
+  constructor(private http: HttpClient) {
+  }
+
+  public ngOnInit() {
+    this.phoneList$ = this.http.get('telephones');
+  }
 }
